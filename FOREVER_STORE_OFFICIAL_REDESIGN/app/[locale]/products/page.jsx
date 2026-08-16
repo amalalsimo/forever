4a2a053c3652e20063050521f655eb13120dd830
amalalsimo <amalalforever@gmail.com>
2026-08-16 21:25:08 +0100

    Delete FOREVER_STORE_OFFICIAL_REDESIGN directory

--- FOREVER_STORE_OFFICIAL_REDESIGN/app/[locale]/products/page.jsx
+++
@@ -1,8 +0,0 @@
-import ProductsExplorer from '../../../components/ProductsExplorer';
-import { getDictionary } from '../../../lib/i18n';
-import { products } from '../../../lib/products';
-
-export default async function ProductsPage({ params }) {
-  const { locale } = await params;
-  return <ProductsExplorer locale={locale} t={getDictionary(locale)} products={products} />;
-}
