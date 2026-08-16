2e6ed732531823a2ce85746859b3be24cd64d6ac
amalalsimo <amalalforever@gmail.com>
2026-08-16 21:24:53 +0100

    Delete FOREVER_STORE_FINAL_OFFICIAL_STYLE/forever-store directory

--- FOREVER_STORE_FINAL_OFFICIAL_STYLE/forever-store/app/[locale]/cart/page.jsx
+++
@@ -1,7 +0,0 @@
-import CartPageClient from '../../../components/CartPageClient';
-import { getDictionary } from '../../../lib/i18n';
-
-export default async function CartPage({ params }) {
-  const { locale } = await params;
-  return <CartPageClient locale={locale} t={getDictionary(locale)} />;
-}
--- FOREVER_STORE_FINAL_OFFICIAL_STYLE/forever-store/app/[locale]/checkout/page.jsx
+++
@@ -1,7 +0,0 @@
-import CheckoutClient from '../../../components/CheckoutClient';
-import { getDictionary } from '../../../lib/i18n';
-
-export default async function CheckoutPage({ params }) {
-  const { locale } = await params;
-  return <CheckoutClient locale={locale} t={getDictionary(locale)} />;
-}
